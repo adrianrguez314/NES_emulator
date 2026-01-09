4008a7762e51189bc48cac7131e2cdeacc1b0026
adrianrguez314 <177678808+adrianrguez314@users.noreply.github.com>
2026-01-09 21:13:02 +0000

    Fix minor bugs and error opcode hex instructions

--- src/cpu/opcodes.h
+++ src/cpu/opcodes.h
@@ -8,11 +8,11 @@
 namespace Ops {
     constexpr uint8_t NOP = 0xEA;
 
-    constexpr uint8_t LDA_IMM = 0xA9, LDA_ZP = 0xA5, LDA_ZPX = 0xB5, LDA_ABS = 0xAD, LDA_ABSX = 0xBD, LDA_ABSY = 0xB9, LDA_INX = 0xA1, LDA_INY = 0xB2;
+    constexpr uint8_t LDA_IMM = 0xA9, LDA_ZP = 0xA5, LDA_ZPX = 0xB5, LDA_ABS = 0xAD, LDA_ABSX = 0xBD, LDA_ABSY = 0xB9, LDA_INX = 0xA1, LDA_INY = 0xB1;
     constexpr uint8_t LDX_IMM = 0xA2, LDX_ZP = 0xA6, LDX_ZPY = 0xB6, LDX_ABS = 0xAE, LDX_ABSY = 0xBE;
     constexpr uint8_t LDY_IMM = 0xA0, LDY_ZP = 0xA4, LDY_ZPX = 0xB4, LDY_ABS = 0xAC, LDY_ABSX = 0xBC;
 
-    constexpr uint8_t STA_ZP = 0x85, STA_ZPX = 0x95, STA_ABS = 0x8D, STA_ABSX = 0x90, STA_ABSY = 0x99, STA_INX = 0x81, STA_INY = 0x92;
+    constexpr uint8_t STA_ZP = 0x85, STA_ZPX = 0x95, STA_ABS = 0x8D, STA_ABSX = 0x9D, STA_ABSY = 0x99, STA_INX = 0x81, STA_INY = 0x91;
     constexpr uint8_t STX_ZP = 0x86, STX_ZPY = 0x96, STX_ABS = 0x8E;
     constexpr uint8_t STY_ZP = 0x84, STY_ZPX = 0x94, STY_ABS = 0x8C;
 
